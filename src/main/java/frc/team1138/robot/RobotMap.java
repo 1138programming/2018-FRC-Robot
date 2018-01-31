8c1d838cea5b47687836dd5f8a6494b537f1016a
crgideon19 <30914786+crgideon19@users.noreply.github.com>
2018-01-30 16:32:13 -0800

    Lift branch (#1)
    * Lift Subsystem
    
    Lift Subsystem was defined in this branch.
    
    * Changes to lift subsystem
    
    Just making it look a little nicer and I added a TODO
    
    * Changed Little Thing
    
    Drive Base Stuff Boi, like the master
    
    * New DriveWithLift Command
    
    I made a command to drive the lift using a joystick axis.

--- src/main/java/frc/team1138/robot/RobotMap.java
+++ src/main/java/frc/team1138/robot/RobotMap.java
@@ -1,21 +1,22 @@
 package frc.team1138.robot;
 
 /**
- * The RobotMap is a mapping where the ports, sensors and actuators are wired into
- * a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
+ * The RobotMap is a mapping where the ports, sensors and actuators are wired
+ * into a variable name. This provides flexibility changing wiring, makes
+ * checking the wiring easier and significantly reduces the number of magic
+ * numbers floating around.
  * 
  * NOTE: We are localizing all variables this year. Therefore, to find their
  * values look in the local files that they are used in. They are in this code
  * as well just in case you can't find something, but are all commented out and
  * should not be changed from here, as it will have no effect.
  */
-public class RobotMap {	
+public class RobotMap
+{
 
 	/**
-	This comment box is all of the mapping we are using for ...
-	
-	
-	**/
+	 * This comment box is all of the mapping we are using for ...
+	 * 
+	 * 
+	 **/
 }
--- src/main/java/frc/team1138/robot/subsystems/Collector.java
+++ src/main/java/frc/team1138/robot/subsystems/Collector.java
@@ -5,21 +5,20 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.team1138.robot.commands.DriveWithJoysticks;
 
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
-import com.ctre.phoenix. motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 /**
  *
  */
 public class Collector extends Subsystem
 {
-	//Declaring the talons, limit switch and solenoid
+	// Declaring the talons, limit switch and solenoid
 	private TalonSRX rightCollector, leftCollector;
 	private Solenoid plunger;
 	private DigitalInput cubPossLimit;
-	
+
 	public static final int KLeftCollectorMotor = 10;
 	public static final int KRightCollectorMotor = 11;
 	public static final int KCubPossLimit = 0;
@@ -27,27 +26,28 @@ public class Collector extends Subsystem
 
 	public Collector()
 	{
-        //setting the limit switch
+		// setting the limit switch
 		cubPossLimit = new DigitalInput(KCubPossLimit);
-		
-		//Setting up base talons
+
+		// Setting up base talons
 		rightCollector = new TalonSRX(KRightCollectorMotor);
 		leftCollector = new TalonSRX(KLeftCollectorMotor);
-		
-		//Configuring the masters
+
+		// Configuring the masters
 		rightCollector.setInverted(true);
 
-		//Configuring the slaves
-		leftCollector.set(ControlMode.Follower,rightCollector.getDeviceID());
-		
-		// Solenoid 
+		// Configuring the slaves
+		leftCollector.set(ControlMode.Follower, rightCollector.getDeviceID());
+
+		// Solenoid
 		plunger = new Solenoid(KPlunger);
 	}
 
-	public void initDefaultCommand(){
-		//Set the default command for a subsystem here.
-//			setDefaultCommand(new DriveWithJoysticks());
+	public void initDefaultCommand()
+	{
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new DriveWithJoysticks());
 	}
-	
-//
+
+	//
 }
