d3bcc2343976f21c524629c558de5d1659f7084f
Eagle Engineering <eeuser@chaminet.net>
2018-02-03 20:28:17 -0800

    Changes To Arm Branch
    There is an unfinished logic problem within this branch, so we're gonna try to figure it out next time.

--- src/main/java/frc/team1138/robot/subsystems/Arm.java
+++ src/main/java/frc/team1138/robot/subsystems/Arm.java
@@ -1,10 +1,9 @@
 package frc.team1138.robot.subsystems;
 
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.DigitalInput;
-
-
-
+import edu.wpi.first.wpilibj.PIDController;
 import frc.team1138.robot.RobotMap;
 import frc.team1138.robot.commands.DriveWithJoysticks;
 import frc.team1138.robot.commands.MoveArmWithJoysticks;
@@ -12,7 +11,9 @@ import frc.team1138.robot.commands.MoveArmWithJoysticks;
 import com.ctre.phoenix.sensors.PigeonIMU;
 
 import java.sql.ResultSet;
+import java.util.function.ToDoubleBiFunction;
 
+import com.ctre.phoenix.ParamEnum;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -20,11 +21,12 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 /**
  *
  */
-public class Arm extends Subsystem
+public class Arm extends PIDSubsystem
 {
 	//Declaring the talons, digital limits, and encoder
 	private TalonSRX armMotor;
 	private DigitalInput armLowerLimit, armUpperLimit;
+	private PIDController armController;
 
 	//Making variables for the arm talon 
 	public static final int KArmMotor = 7;
@@ -33,66 +35,102 @@ public class Arm extends Subsystem
 	public static final int KArmUpperLimit = 2;
 	//setting dead zone limit
 	public static final double KDeadZoneLimit = 0.2;
-	public static final int KLowSpeed = //TODO set variable;
-	public static final int KLowValue = //TODO set variable;
-	public static final int KZeroSpeed = //TODO set variable;
+	public static final double KLowSpeed = 0.4; //TODO set variable;
+	public static final int KLowValue = 5; //TODO set variable;
+	public static final int KZeroSpeed = 0; //TODO set variable;
 	public static final int KEncoderValue = 1000;
-	public static final int KTargetEncoderValue =//TODO set variable;
-	
-
-
+	public static final int KTicksPerRotation = 4096;
 	
 	public Arm()
 	{
+		super("Arm PID", 0, 0, 0); // TODO mess with P, I, and D
+		setAbsoluteTolerance(100); // Threshold/error allowed TODO change this value to correct value
+		getPIDController().setContinuous(true); // Change based on need, probably should be continuous
+//		getPIDController().setInputRange(minimumInput, maximumInput); // TODO figure out after getting the bot
+		getPIDController().setOutputRange(-1.0, 1.0);
+		
 		//Setting up the arm motor talon
 		armMotor = new TalonSRX(KArmMotor);
 		
 		//setting up the limit digital input
-		armLowerLimit = new DigitalInput(KArmLowerLimit); 
+		armLowerLimit = new DigitalInput(KArmLowerLimit);
 		armUpperLimit = new DigitalInput(KArmUpperLimit);
 		
 		//setting up encoders
 		armMotor.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,0,0);
-	
-
-	
+		
+		//Setting up PID controller
+		armController.enable();
+		armMotor.configSetParameter(ParamEnum.eClearPositionOnLimitF, 1, 0, 0, 10); //TODO I don't know if this is quite the right function to clear the encoder when it touches the limit, so TODO figure out the true value
 	}
 	
+	//Sets the default command
 	public void initDefaultCommand()
 	{
 		setDefaultCommand(new MoveArmWithJoysticks());
 	}
 	
-	public void MoveArm (double armAxis)
+	public void MoveArm(double armAxis)
 	{
 		if(armAxis > KDeadZoneLimit || armAxis < -KDeadZoneLimit)
 		{
-			armMotor.set(ControlMode.PercentOutput, armAxis);
+			getPIDController().setSetpoint(getPosition() + armAxis*1000); //TODO experiment with this last constant value
 		}
 		else
 		{
-			armMotor.set(ControlMode.PercentOutput, 0);
-			
+			getPIDController().setSetpoint(getPosition());
 		}
-		
 	}
-	
 	
 	public void moveArmToLimitSwitch(double encoderValue, float armSpeed)
 	{
-		if (armMotor.getSensorCollection().getQuadraturePosition() < KTargetEncoderValue)
+		if(armLowerLimit.get() == true)
 		{
-			if (armMotor.getSensorCollection().getQuadraturePosition() < KLowValue) {
-				armMotor.set(ControlMode.PercentOutput, KLowSpeed);
-			}
-			else {
-				armMotor.set(ControlMode.PercentOutput, armSpeed);
-			}
+			resetEncoders(); //TODO SOLVE THIS!!!!!
 		}
-		else {
-			armMotor.set(ControlMode.PercentOutput, KZeroSpeed); 
-
-		}
-		}
+		
+//		if (armMotor.getSensorCollection().getQuadraturePosition() < encoderValue)
+//		{
+//			if (armMotor.getSensorCollection().getQuadraturePosition() < KLowValue*KTicksPerRotation)
+//			{
+//				armMotor.set(ControlMode.PercentOutput, KLowSpeed);
+//			}
+//			else
+//			{
+//				armMotor.set(ControlMode.PercentOutput, armSpeed);
+//			}
+//		}
+//		else {
+//			armMotor.set(ControlMode.PercentOutput, KZeroSpeed);
+//
+//		}
+	}
 	
+	public void moveArmWithEncoders(double position)
+	{
+		getPIDController().setSetpoint(position*KTicksPerRotation);
+	}
+
+	@Override
+	protected double returnPIDInput()
+	{
+		return returnEncoderValue();
+	}
+
+	@Override
+	protected void usePIDOutput(double output)
+	{
+		armMotor.set(ControlMode.PercentOutput, output);
+	}
+	
+	public double returnEncoderValue()
+	{
+		return armMotor.getSensorCollection().getQuadraturePosition();
+	}
+	
+	@Override
+	public boolean onTarget() {
+		// TODO Auto-generated method stub
+		return super.onTarget();
+	}
 }
